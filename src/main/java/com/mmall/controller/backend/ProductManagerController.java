2264660c8d21c9fe27b85d147bcffb78a4e6b3cd
znlehandwyj <zhuge1973vyh@126.com>
2021-06-16 15:36:07 +0800

    购物车模块完善

--- src/main/java/com/mmall/controller/backend/ProductManagerController.java
+++ src/main/java/com/mmall/controller/backend/ProductManagerController.java
@@ -12,7 +12,6 @@ import com.mmall.service.IUserService;
 import com.mmall.util.PropertiesUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ src/main/java/com/mmall/util/BigDecimalUtil.java
@@ -0,0 +1,34 @@
+package com.mmall.util;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Steven.Lu on 2018/7/17.
+ */
+public class BigDecimalUtil {
+    private BigDecimalUtil(){}
+    //加法
+    public static BigDecimal add(double v1,double v2){
+        BigDecimal b1=new BigDecimal(Double.toString(v1));
+        BigDecimal b2=new BigDecimal(Double.toString(v2));
+        return b1.add(b2);
+    }
+    //减法
+    public static BigDecimal sub(double v1,double v2){
+        BigDecimal b1=new BigDecimal(Double.toString(v1));
+        BigDecimal b2=new BigDecimal(Double.toString(v2));
+        return b1.subtract(b2);
+    }
+    //乘法
+    public static BigDecimal mul(double v1,double v2){
+        BigDecimal b1=new BigDecimal(Double.toString(v1));
+        BigDecimal b2=new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2);
+    }
+    //除法
+    public static BigDecimal div(double v1,double v2){
+        BigDecimal b1=new BigDecimal(Double.toString(v1));
+        BigDecimal b2=new BigDecimal(Double.toString(v2));
+        return b1.divide(b2,2,BigDecimal.ROUND_HALF_UP);     //四舍五入，保留两位小数
+    }
+}
